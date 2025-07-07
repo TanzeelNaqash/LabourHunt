d3b95e72f9fd8a32a377d04f83cbe11e32edae1d
Tanzeel Naqash <tanzeelnaqash987@gmail.com>
2025-07-07 14:59:35 +0530

    Fix: queryclient error fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClientProvider } from "@tanstack/react-query"
 import App from './App.jsx'
 import './index.css'
-import { queryClient } from "./lib/queryClient.jsx"
+import { queryClient } from "./lib/queryClient"
 import { AuthProvider } from "./providers/AuthProvider"
 
 
